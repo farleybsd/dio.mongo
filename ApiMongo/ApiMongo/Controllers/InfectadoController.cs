74c1d729bf9a9bebd369697be32fa7f438b804a2
farleybsd <farley.t.i@hotmail.com>
2023-11-24 15:01:14 -0300

    b1

---
+++ ApiMongo/ApiMongo/Controllers/InfectadoController.cs
@@ -0,0 +1,39 @@
+﻿using ApiMongo.Data;
+using ApiMongo.Data.Collections;
+using ApiMongo.Model;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ApiMongo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        MongoDb _mongoDB;
+
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(MongoDb mongoDB)
+        {
+          _mongoDB = mongoDB;
+          _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectato = new Infectado(dto.DataNascimento,dto.Sexo,dto.Latitude,dto.Longitude);
+            _infectadosCollection.InsertOne(infectato);
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
