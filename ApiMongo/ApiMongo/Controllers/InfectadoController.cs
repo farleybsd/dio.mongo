18d6ec4d6c1dc6bfa975a288282cc17d026882dc
farleybsd <farley.t.i@hotmail.com>
2023-11-27 11:51:29 -0300

    CRUD

--- ApiMongo/ApiMongo/Controllers/InfectadoController.cs
+++ ApiMongo/ApiMongo/Controllers/InfectadoController.cs
@@ -16,14 +16,14 @@ namespace ApiMongo.Controllers
 
         public InfectadoController(MongoDb mongoDB)
         {
-          _mongoDB = mongoDB;
-          _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
         }
 
         [HttpPost]
         public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
         {
-            var infectato = new Infectado(dto.DataNascimento,dto.Sexo,dto.Latitude,dto.Longitude);
+            var infectato = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
             _infectadosCollection.InsertOne(infectato);
             return StatusCode(201, "Infectado adicionado com sucesso");
         }
@@ -34,6 +34,21 @@ namespace ApiMongo.Controllers
             var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
 
             return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtuluizarSexo([FromBody] InfectadoDto dto)
+        {
+            var infectato = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+            return Ok("Atualizado Com Sucesso");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] DateTime dataNasc)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dataNasc));
+            return Ok("Deletado Com Sucesso");
         }
     }
 }
